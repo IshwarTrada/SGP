6ced5ce92ee94b7fe4fc81f8bf7ba4151a6de44a
Ishwar <ishwartrada15@gmail.com>
2024-08-17 23:05:34 +0530

    chore: Add shipping schema to order model and update order schema

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,47 +1,107 @@
 import mongoose, { Schema } from "mongoose";
 
+const shippingSchema = new Schema({
+  fname: {
+    type: String,
+    required: true,
+  },
+  lname: {
+    type: String,
+    required: true,
+  },
+  companyName: {
+    type: String,
+    default: "",
+  },
+
+  shippingAddress: {
+    type: String,
+    required: true,
+  },
+  zip: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /\+91[0-9]{10}/.test(v); // Ensures the phone number starts with +91 followed by 10 digits
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
+});
+
 const orderSchema = new Schema(
   {
-    user: {
+    userId: {
       type: Schema.Types.ObjectId,
       ref: "User",
+      required: true,
+      index: true,
     },
-    product: [{ required: true }],
-    shippingAddress1: {
+    orderId: {
       type: String,
       required: true,
+      index: true,
+      unique: true,
     },
-    shippingAddress2: {
-      type: String,
-    },
-    city: {
-      type: String,
-      required: true,
-    },
-    zip: {
-      type: String,
-      required: true,
-    },
-    country: {
-      type: String,
-      required: true,
-      default: "India",
-    },
-    phone: {
-      type: String,
-      required: true,
-    },
-    status: {
-      type: String,
-      required: true,
-      default: "Pending",
-    },
+
+    products: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        productSubTotal: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
     totalPrice: {
       type: Number,
       required: true,
     },
+
+    status: {
+      type: String,
+      required: true,
+      enum: ["Confirmed", "In Transit", "Delivered"],
+      default: "Confirmed",
+      index: true,
+    },
+
+    shippingInfo: shippingSchema,
+
+    extraNotes: {
+      type: String,
+      default: "",
+    },
   },
   { timestamps: true }
 );
+
+// Compound index to optimize queries based on multiple fields
+orderSchema.index({ userId: 1, status: 1 });
 
 export const Order = mongoose.model("Order", orderSchema);
